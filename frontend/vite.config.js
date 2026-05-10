990909514132f3eaa5abfe8806e86ee70f6db053
Mahmoud Dardiry <big6er00@gmail.com>
2026-05-10 16:58:27 +0300

    Fix public assets not deployed on Linux (Public vs public)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || '/',
+  publicDir: 'Public',
   server: {
     port: 3000,
     proxy: {
